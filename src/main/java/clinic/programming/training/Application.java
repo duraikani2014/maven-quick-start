30d157105690279882247a9ae00b610bffb05182
Durairaj <duraikani2014@gmail.com>
2024-02-19 23:22:01 +0530

    dd

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,16 @@
 package clinic.programming.training;
 
 public class Application {
+
+    public void greeting(){
+        List<String> list=new ArrayList<String>();
+        list.add("Hello Welcome");
+
+        for(String str:list){
+            System.out.println(str);
+        }
+
+    }
     
     public Application() {
         System.out.println ("Inside Application");
